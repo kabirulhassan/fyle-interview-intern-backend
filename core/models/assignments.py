5f5e6ea4a00a6a6f8492dabc9fa48fd46ae6ba45
kabirulhassan <kabirulhassan7@gmail.com>
2023-04-06 00:32:18 +0530

    add get assignments by teacher

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -65,6 +65,7 @@ class Assignment(db.Model):
         assertions.assert_found(assignment, 'No assignment with this id was found')
         assertions.assert_valid(assignment.student_id == principal.student_id, 'This assignment belongs to some other student')
         assertions.assert_valid(assignment.content is not None, 'assignment with empty content cannot be submitted')
+        assertions.assert_valid(assignment.state == AssignmentStateEnum.DRAFT, 'only a draft assignment can be submitted')
 
         assignment.teacher_id = teacher_id
         assignment.state = AssignmentStateEnum.SUBMITTED
@@ -75,3 +76,21 @@ class Assignment(db.Model):
     @classmethod
     def get_assignments_by_student(cls, student_id):
         return cls.filter(cls.student_id == student_id).all()
+    
+    @classmethod
+    def get_assignments_by_teacher(cls, teacher_id):
+        return cls.filter(cls.teacher_id == teacher_id).all()
+    
+    @classmethod
+    def grade_assignment(cls, _id, grade, principal: Principal):
+        assignment = Assignment.get_by_id(_id)
+        assertions.assert_found(assignment, 'No assignment with this id was found')
+        assertions.assert_valid(assignment.teacher_id == principal.teacher_id, 'This assignment belongs to some other teacher')
+        assertions.assert_valid(assignment.state == AssignmentStateEnum.SUBMITTED, 'only submitted assignment can be graded')
+        assertions.assert_valid(grade in GradeEnum.__members__, 'invalid grade')
+        assignment.state = AssignmentStateEnum.GRADED
+        assignment.grade = grade
+        db.session.commit()
+        db.session.flush()
+
+        return assignment
